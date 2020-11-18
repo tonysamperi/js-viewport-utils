77e62008a21daf0c6ab5a4ebad9e4f39743236ae
Tony Samperi <github@tonysamperi.it>
2020-11-18 18:16:03 +0100

    MAINTENANCE * Fixed build * Changed rollup config to js file

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import sourceMaps from "rollup-plugin-sourcemaps";
-import camelCase from "lodash.camelcase";
 import typescript from "rollup-plugin-typescript2";
 import json from "rollup-plugin-json";
 
 const pkg = require("./package.json");
-
 const libraryName = "js-viewport-utils";
+const camelize = (str) => {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, "");
+};
 
 export default {
     input: `src/${libraryName}.ts`,
     output: [
-        {file: pkg.main, name: camelCase(libraryName), format: "umd", sourcemap: true},
+        {file: pkg.main, name: camelize(libraryName), format: "umd", sourcemap: true},
         {file: pkg.module, format: "es", sourcemap: true},
     ],
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
