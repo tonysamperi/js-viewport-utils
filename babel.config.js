6840772ea49bd25e231610426431002241352fb6
Tony Samperi <github@tonysamperi.it>
2020-11-19 11:47:05 +0100

    MAINTENANCE * Babel config 2

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-{
-  "presets": ["@babel/env", "@babel/typescript"],
-  "plugins": [
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread"
-  ]
-}
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    +    '@babel/preset-typescript',
+  ],
+};
